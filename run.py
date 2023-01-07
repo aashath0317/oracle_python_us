d90b5c94b1242fe5d904b03ab53d55f5741359da
aashath0317 <89287795+aashath0317@users.noreply.github.com>
2023-01-07 14:30:53 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,4 +1,6 @@
 import oci
+import os
+import time
 config = {
     "user": "ocid1.user.oc1..aaaaaaaaul4cbqzowr4x5bnmsdmlqpo3onbs2ekwqb55yhnbh3izmksgr5ha",
     "fingerprint": "4a:d4:ae:62:e3:29:bc:a6:0c:c9:b6:55:7a:f7:ba:a9",
@@ -6,38 +8,44 @@ config = {
     "tenancy": "ocid1.tenancy.oc1..aaaaaaaasnvm3ufpikguqeurze3z6ek5pgbsg2uiqtkfqha5qxplzbqqbm5a",
     "region": "ap-singapore-1"
 }
-instance_name = ''
-shape = ''
+
+instance_name = 'instance-20230107-1323'
+shape = 'VM.Standard.E2.1.Micro'
 availability_domain = 'vOeV:AP-SINGAPORE-1-AD-1'
 compartment_id= 'ocid1.tenancy.oc1..aaaaaaaasnvm3ufpikguqeurze3z6ek5pgbsg2uiqtkfqha5qxplzbqqbm5a'
-image_id= 'ocid1.image.oc1.ap-singapore-1.aaaaaaaav2nfrctnfspilj3ueob37g2yljyr7lhv663mpargajcunyqzjnla'
-compute_client = oci.core.ComputeClient(config)
-subnet_id = 'ocid1.vcn.oc1.ap-singapore-1.amaaaaaat3xrswia6uzcrxayg4hlmxiwmabdn7rjiliqfcbwkerg32eptoua'
+image_id= 'ocid1.image.oc1.ap-singapore-1.aaaaaaaaylr7uotjuy4rbyfe2tm6icj6vwr77goh3rzvmcwzxovunkejypsa'
+subnet_id = 'ocid1.subnet.oc1.ap-singapore-1.aaaaaaaavnabp2lchaixly3e4idw4kzhan3hng73sou4voxnr5zodncfwsnq'
 ssh_authorized_keys = 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCxs3qA5wRuR2UdQdcU4hgI2OmOko4rtT2vWS5SXWZCHemUJLHya5YAq2nnn9wOA3V9mgBDUYkyui+tB/56lR7wipkUUeJeIcbgSMk7/ZRAd+4XCZP93QWhj0Jh+tBaOOjrV04s4O8oQs0Vfvjs11FkAhITcc8EhkPIiAsDhirdY3EbThP3iGOrQUGoOsTSHAOi09MOROd/ecNXdfYVCot9tEdRpydsRHhchs7pkvY0px+n85V964bYuElwVOec+h2N68eqG8+sdmrJYEccxiaC6ezA9EnGETFdH89mfgVjZ3U+SxppisnxtmFxPaqncZQ2QqK6qeLN1jdUeR1PR/qn rsa-key-20230107'
+bootVolumeSizeInGBs = 50
+bootVolumeVpusPerGB = 10
+isPvEncryptionInTransitEnabled = True
 
-
+compute_client = oci.core.ComputeClient(config)
 while True:
   # Create a VM
-  vm_response = compute_client.launch_instance(
-      oci.core.models.LaunchInstanceDetails(
-          compartment_id=compartment_id,
-          display_name = instance_name,
-          image_id=image_id,
-          shape=shape,
-          subnet_id=subnet_id,
-          availability_domain=availability_domain,
-          is_pv_encryption_in_transit_enabled=True,
-          metadata={
-              "ssh_authorized_keys": ssh_authorized_keys
-          }
-      )
-  )
   try:  
-      vm = vm_response.data
-      print("Created VM:", vm.id)
-  except ServiceError:
-      print ("Too many tries retrying in 10")   
-      continue 
+    vm_response = compute_client.launch_instance(
+        oci.core.models.LaunchInstanceDetails(
+            compartment_id=compartment_id,
+            display_name = instance_name,
+            image_id=image_id,
+            shape=shape,
+            subnet_id=subnet_id,
+            availability_domain=availability_domain,
+            is_pv_encryption_in_transit_enabled=True,
+            metadata={
+                "ssh_authorized_keys": ssh_authorized_keys
+            }
+        )
+    )
+  except oci.exceptions.ServiceError as er:
+      print(er.message)
+      print("waiting for 10s")
+      time.sleep(10)
+      continue
+  
+  vm = vm_response.data
+  print("Created VM:", vm.id)
   # Wait for the VM to reach the "Running" state
   compute_client.get_instance(vm.id).wait_until(
       oci.core.models.Instance.LIFECYCLE_STATE_RUNNING,
