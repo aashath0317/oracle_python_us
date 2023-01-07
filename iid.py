299b130be8313d53849e4388e54ffb515693e2fd
aashath0317 <89287795+aashath0317@users.noreply.github.com>
2023-01-07 11:55:44 +0530

    Update iid.py

--- iid.py
+++ iid.py
@@ -1,8 +1,14 @@
 import oci
-compartmentId= 'ocid1.tenancy.oc1..aaaaaaaasnvm3ufpikguqeurze3z6ek5pgbsg2uiqtkfqha5qxplzbqqbm5a'
 # Create a client for the Compute Cloud API
+config = {
+    "user": "ocid1.user.oc1..aaaaaaaaul4cbqzowr4x5bnmsdmlqpo3onbs2ekwqb55yhnbh3izmksgr5ha",
+    "fingerprint": "4a:d4:ae:62:e3:29:bc:a6:0c:c9:b6:55:7a:f7:ba:a9",
+    "key_file": "oci_private.pem",
+    "tenancy": "ocid1.tenancy.oc1..aaaaaaaasnvm3ufpikguqeurze3z6ek5pgbsg2uiqtkfqha5qxplzbqqbm5a",
+    "region": "ap-singapore-1"
+    "compartment_id": "ocid1.tenancy.oc1..aaaaaaaasnvm3ufpikguqeurze3z6ek5pgbsg2uiqtkfqha5qxplzbqqbm5a"
+}
 compute_client = oci.core.ComputeClient(config)
-
 # List the available images
 images = compute_client.list_images(compartment_id).data
 
